c656fb32b6936115e1efdf6a2abeff7c3ab207c6
hayabusa <10593623+m-hayabusa@users.noreply.github.com>
2022-07-11 12:02:53 +0900

    プロセスが複数起動する可能性がある問題を修正 (#6)

---
+++ utils/regist.ps1
@@ -0,0 +1,8 @@
+Set-Location $PSScriptRoot\..
+
+$tri = New-ScheduledTaskTrigger -AtLogOn -User $((Get-WMIObject -class Win32_ComputerSystem).UserName)
+$pri = New-ScheduledTaskPrincipal -LogonType S4U -RunLevel Limited -UserId ((Get-WMIObject -class Win32_ComputerSystem).UserName)
+$act = New-ScheduledTaskAction -WorkingDirectory $(Get-Location) -Execute """$((Get-Command Node).Source)""" -Argument "main.js"
+
+Register-ScheduledTask -TaskName VRChat-Exif-Writer -TaskPath nekomimiStudio -Trigger $tri -Principal $pri -Action $act -Force
+Start-Sleep -s 1
---
+++ utils/unregist.ps1
@@ -0,0 +1,2 @@
+Unregister-ScheduledTask -Confirm:$false -TaskName VRChat-Exif-Writer
+Start-Sleep -s 1
