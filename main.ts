423a032dfa4bdc8dc5f535b9f333a992b2050173
hayabusa <s.hashimoto@hs-sh.net>
2022-12-01 18:51:07 +0900

    Squashed commit of the following:
    commit 52e3a28c26a2755f0bc4e4a6d52ffbff98592eb0
    Author: hayabusa <s.hashimoto@hs-sh.net>
    Date:   Thu Dec 1 18:50:29 2022 +0900
    
        permissionがgroupであるインスタンスに対応

--- main.ts
+++ main.ts
@@ -266,7 +266,7 @@ class logReader {
                 }
             }
             {
-                const match = line.match(/.*\[Behaviour\] Joining (wrld_.*?):(?:.*?(private|friends|hidden)\((.*?)\))?/);
+                const match = line.match(/.*\[Behaviour\] Joining (wrld_.*?):(?:.*?(private|friends|hidden|group)\((.*?)\))?/);
                 if (match) {
                     roomInfo = new RoomInfo();
                     roomInfo.world_id = match[1];
