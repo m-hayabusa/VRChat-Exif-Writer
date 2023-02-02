46080c9148604b6602bdfadaf3b8809b77549987
hayabusa <s.hashimoto@hs-sh.net>
2023-02-02 14:06:46 +0900

    ソースファイルのディレクトリを切った

--- utils/regist.ps1
+++ utils/regist.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot\..
 
 $tri = New-ScheduledTaskTrigger -AtLogOn -RandomDelay $(New-TimeSpan -Minutes 1) -User $((Get-WMIObject -class Win32_ComputerSystem).UserName)
 $pri = New-ScheduledTaskPrincipal -LogonType S4U -RunLevel Limited -UserId ((Get-WMIObject -class Win32_ComputerSystem).UserName)
-$act = New-ScheduledTaskAction -WorkingDirectory $(Get-Location) -Execute """$((Get-Command Node).Source)""" -Argument "main.js"
+$act = New-ScheduledTaskAction -WorkingDirectory $(Get-Location) -Execute """$((Get-Command Node).Source)""" -Argument "built/main.js"
 
 Register-ScheduledTask -TaskName VRChat-Exif-Writer -TaskPath nekomimiStudio -Trigger $tri -Principal $pri -Action $act -Force
 Start-Sleep -s 1
